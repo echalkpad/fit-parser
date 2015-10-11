50d4bb864639f29a328b5473ff5ae63482ae86c7
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-10-11 21:28:30 +0300

    less tostring

--- src/reader.js
+++ src/reader.js
@@ -27,7 +27,7 @@ class Reader {
 		const result = this.buffer.toString('ascii', this.pos, this.pos + length)
 		this.pos += length;
 
-		return result.toString();
+		return result;
 	}
 
 	UInt16BE() {
